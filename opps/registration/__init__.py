fba3f0e1195d62374156a8023ede5df5331b7abc
Bruno Rocha <rochacbruno@gmail.com>
2013-04-14 03:28:42 +0000

    changed to opps app structure

---
+++ opps/registration/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+VERSION = (0, 1, 1)
+
+__version__ = ".".join(map(str, VERSION))
+__status__ = "Development"
+__description__ = u"Registration App for Opps CMS"
+
+__author__ = u"Bruno Cezar Rocha"
+__credits__ = []
+__email__ = u"rochacbruno@gmail.com"
+__license__ = u"YACOWS LICENSE"
--- setup.py
+++ setup.py
@@ -1,49 +1,89 @@
-from distutils.core import setup
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
 import os
+import fnmatch
+import glob
 
-from registration import get_version
+from setuptools import setup, find_packages
 
-
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir:
-    os.chdir(root_dir)
-
-for dirpath, dirnames, filenames in os.walk('registration'):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'):
-            del dirnames[i]
-    if '__init__.py' in filenames:
-        pkg = dirpath.replace(os.path.sep, '.')
-        if os.path.altsep:
-            pkg = pkg.replace(os.path.altsep, '.')
-        packages.append(pkg)
-    elif filenames:
-        prefix = dirpath[13:]  # Strip "registration/" or "registration\"
-        for f in filenames:
-            data_files.append(os.path.join(prefix, f))
+from opps import registration
 
 
-setup(name='django-registration',
-      version=get_version().replace(' ', '-'),
-      description='An extensible user-registration application for Django',
-      author='Bruno Cezar Rocha',  # forked from James Bennett <james@b-list.org>
-      author_email='rochacbruno@gmail.com',
-      url='https://github.com/rochacbruno/django-registration',
-      download_url='https://github.com/rochacbruno/django-registration/tarball/master',
-      package_dir={'registration': 'registration'},
-      packages=packages,
-      package_data={'registration': data_files},
-      classifiers=['Development Status :: 4 - Beta',
-                   'Environment :: Web Environment',
-                   'Framework :: Django',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: BSD License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Topic :: Software Development :: Libraries :: Python Modules',
-                   'Topic :: Utilities'],
-      )
+install_requires = ["opps"]
+
+classifiers = ["Development Status :: 4 - Beta",
+               "Intended Audience :: Developers",
+               "License :: OSI Approved :: MIT License",
+               "Operating System :: OS Independent",
+               "Framework :: Django",
+               'Programming Language :: Python',
+               "Programming Language :: Python :: 2.7",
+               "Operating System :: OS Independent",
+               "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+               'Topic :: Software Development :: Libraries :: Python Modules']
+
+try:
+    long_description = open('README.md').read()
+except:
+    long_description = registration.__description__
+
+
+## ????
+def opj(*args):
+    path = os.path.join(*args)
+    return os.path.normpath(path)
+
+
+def find_data_files(srcdir, *wildcards, **kw):
+    # get a list of all files under the srcdir matching wildcards,
+    # returned in a format to be used for install_data
+    def walk_helper(arg, dirname, files):
+        if '.svn' in dirname:
+            return
+        names = []
+        lst, wildcards = arg
+        for wc in wildcards:
+            wc_name = opj(dirname, wc)
+            for f in files:
+                filename = opj(dirname, f)
+
+                if fnmatch.fnmatch(filename, wc_name) and not os.path.isdir(filename):
+                    names.append(filename)
+        if names:
+            lst.append((dirname, names))
+
+    file_list = []
+    recursive = kw.get('recursive', True)
+    if recursive:
+        os.path.walk(srcdir, walk_helper, (file_list, wildcards))
+    else:
+        walk_helper((file_list, wildcards),
+                    srcdir,
+                    [os.path.basename(f) for f in glob.glob(opj(srcdir, '*'))])
+    return file_list
+
+data_files = find_data_files('opps/registration/', '*.*')
+
+## ????
+
+setup(
+    name='opps-registration',
+    namespace_packages=['opps', 'opps.registration'],
+    version=registration.__version__,
+    description=registration.__description__,
+    long_description=long_description,
+    classifiers=classifiers,
+    keywords='registration opps cms django apps magazines websites',
+    author=registration.__author__,
+    author_email=registration.__email__,
+    url='http://oppsproject.org',
+    download_url="https://github.com/opps/opps-registration/tarball/master",
+    license=registration.__license__,
+    packages=find_packages(exclude=('doc', 'docs',)),
+    package_dir={'opps': 'opps'},
+    install_requires=install_requires,
+    include_package_data=True,
+    # packages=packages,
+    package_data={'registration': data_files},
+)
